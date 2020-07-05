133c2a3a4fbfb12e966fee90c9ce1e3f49a39cc2
anujjj <anuj10pande@gmail.com>
2020-07-05 21:37:08 +0530

    added server

---
+++ track-server/src/models/Track.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const pointSchema = new mongoose.Schema({
+  timestamp: Number,
+  coords: {
+    latitude: Number,
+    longitude: Number,
+    altitude: Number,
+    accuracy: Number,
+    heading: Number,
+    speed: Number
+  }
+});
+const trackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  distanceTravelled: {
+    type: Number,
+    default: 0
+  },
+  locations: [pointSchema]
+});
+
+mongoose.model('Track', trackSchema);
