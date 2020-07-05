f62b883ab0c8342c2226a1cebd7ef4496dbd83a4
anujjj <anuj10pande@gmail.com>
2020-07-05 17:29:46 +0530

    removed errors

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://c60cbdd1700e.ngrok.io'
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,53 +1,42 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native'
-import { Text, Input, Button } from 'react-native-elements'
-import Spacer from '../components/Spacer'
+import React, { useContext } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChange={() => setEmail({ email })}
-        autoCapitalize="none"
-        autoCorrect={false}
+      <NavigationEvents onWillBlur={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign Up for Tracker"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign Up"
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChange={() => setPassword({ password })}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
+      <NavLink
+        routeName="Signin"
+        text="Already have an account? Sign in instead!"
       />
-      <Spacer>
-        <Button title="Sign Up" />
-      </Spacer>
     </View>
-  )
-}
+  );
+};
 
 SignupScreen.navigationOptions = () => {
   return {
     headerShown: false
-  }
-}
+  };
+};
 
 const styles = StyleSheet.create({
   container: {
-    // borderColor: 'red',
-    borderWidth: 10,
     flex: 1,
     justifyContent: 'center',
-    marginBottom: 200
+    marginBottom: 250
   }
-})
+});
 
 export default SignupScreen;
